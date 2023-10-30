72a592e306ca693f1267f02eba3430a3b82ad3ea
Abhishek Pandit <abhishekpandit986@gmail.com>
2023-10-30 09:55:53 +0530

    set up

---
+++ BlogWebsite_AppWrite/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+        ratione?
+      </div>
+    </>
+  );
+};
+
+export default App;
